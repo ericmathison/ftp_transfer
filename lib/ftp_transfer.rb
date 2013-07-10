04e339db91f4fc0ccfdb59d366cd89931b33e96f
Eric Mathison <dev@ekmathison.com>
2013-07-10 09:23:30 -0700

    Add transfer verification (md5) before archiving

--- lib/ftp_transfer.rb
+++ lib/ftp_transfer.rb
@@ -1,26 +1,40 @@
 require 'net/ftp'
+require 'digest/md5'
 
 class FtpTransfer
-  def initialize(local_outgoing_directory = '/home/eric/Desktop/sending-orders', archive_dir = '/home/eric/Desktop/archived-orders')
-    @local_outgoing_directory = File.expand_path(local_outgoing_directory)
+  def initialize(local_directory = '/home/eric/Desktop/sending-orders', archive_dir = '/home/eric/Desktop/archived-orders')
+    @local_directory = File.expand_path(local_directory)
     @archived_orders_directory = File.expand_path(archive_dir)
+    @ftp = Net::FTP.new('<host>', '<user>', '<password>')
+    @ftp.passive = true
   end
 
   def upload(remote_directory = 'public_html/new_orders')
-    Dir["#{@local_outgoing_directory}/*.edi"].each do |file|
-      Net::FTP.open('foobar.bazquux.com', 'username-goes-here', 'password-goes-here') do |ftp|
-        ftp.passive = true
-        ftp.chdir(remote_directory)
-        ftp.puttextfile(file)
-        ftp.nlst.select { |server_file| server_file =~ /\.edi$/ }
-          .each { |server_file| archive_local_order(server_file) if File.exists?("#{@local_outgoing_directory}/#{server_file}") }
+    @remote_directory = remote_directory
+    # comment out to try a failed file transfer:
+    @ftp.chdir(@remote_directory)
+    Dir["#{@local_directory}/*.edi"].each do |file|
+      begin
+        @ftp.putbinaryfile(file)
+      rescue
+        raise Net::FTPError, 'files did not transfer successfully'
       end
+      archive_local_order(file) if transfer_success?(file)
     end
+  end
+
+  def download(remote_directory = 'public_html/processed_orders')
   end
 
   private
 
-  def archive_local_order(order_file)
-    FileUtils.move("#{@local_outgoing_directory}/#{order_file}", @archived_orders_directory)
+  def transfer_success?(file)
+    @ftp.size(File.basename(file)) > 0 &&
+    Digest::MD5.hexdigest(@ftp.getbinaryfile(File.basename(file), nil)) ==
+    Digest::MD5.hexdigest(File.read(file))
+  end
+
+  def archive_local_order(file)
+    FileUtils.move(file, @archived_orders_directory) if File.exists?(file)
   end
 end
